7ce38dd5e722e54f56adfdcc22b0c90699655740
hschroedl <hansjrgschroedl@gmail.com>
2017-09-27 17:44:43 +0200

    Add some documentation stuff

---
+++ core/Fluentast.md
@@ -0,0 +1,19 @@
+# Module Fluentast
+
+The module contains FluentAST
+
+# Package at.hschroedl.fluentast
+
+Contains all base stuff
+
+# Package at.hschroedl.fluentast.ast.expression
+
+Contains Expressions.
+
+## Level 2 heading
+
+Text after this heading is also part of documentation for `org.jetbrains.kotlin.demo`
+
+# Package at.hschroedl.fluentast.ast.statement
+
+Useful stuff in another package.
--- core/src/main/kotlin/at.hschroedl.fluentast/ast/expression/ArrayAccess.kt
+++ core/src/main/kotlin/at.hschroedl.fluentast/ast/expression/ArrayAccess.kt
@@ -1,12 +1,30 @@
 package at.hschroedl.fluentast.ast.expression
 
 import org.eclipse.jdt.core.dom.AST
-import org.eclipse.jdt.core.dom.Expression
+import org.eclipse.jdt.core.dom.ArrayAccess
 
-
+/**
+ * A wrapper class for [ArrayAccess]. Subclass of [FluentExpression]
+ *
+ *
+ * Using JDT:
+ * ```java
+ * val exp = ast.newArrayAccess()
+ * exp.array = array.build(ast)
+ * exp.index = index.build(ast)
+ * ```
+ *
+ * Using Fluentast:
+ * ```java
+ * exp().build(ast);
+ * ```
+ * @constructor takes a [FluentExpression] as array and a [FluentExpression] as index
+ *
+ * @see ArrayAccess
+ */
 class FluentArrayAccess internal constructor(private val array: FluentExpression,
                                              private val index: FluentExpression) : FluentExpression() {
-    override fun build(ast: AST): Expression {
+    override fun build(ast: AST): ArrayAccess {
         val exp = ast.newArrayAccess()
         exp.array = array.build(ast)
         exp.index = index.build(ast)
